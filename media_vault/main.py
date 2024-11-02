16fc8b15420d5a21bd07a829cd764125d7cd1e28
Goutham100 <gouthamjagadeesh100@gmail.com>
2024-11-02 18:41:46 +0530

    error handling improvements

--- media_vault/main.py
+++ media_vault/main.py
@@ -1,20 +1,25 @@
-from operator import truediv
 
-from tmdbv3api import TMDb, Movie, Search
+from tmdbv3api import TMDb, Movie
 
 tmdb = TMDb()
 tmdb.api_key = '09d0650e48d242f97c52f1e4f6780f98'
 
-def search_media(cinema):
-
+def search_media(movie_):
     movie = Movie()
-    search_results = movie.search(cinema)
 
-    print(f"Title: {search_results[0].title}")
-    print(f"Overview: {search_results[0].overview}")
-    print(f"Release Date: {search_results[0].release_date}")
-    print(f"Rating: {search_results[0].vote_average}")
-    print()
+    try:
+        search_results = movie.search(movie_)
+
+        if not search_results:
+            print("No Results")
+
+        print(f"Title: {search_results[0].title}")
+        print(f"Overview: {search_results[0].overview}")
+        print(f"Release Date: {search_results[0].release_date}")
+        print(f"Rating: {search_results[0].vote_average}")
+        print()
+    except Exception as e:
+        print("No Results")
 
 if __name__ == '__main__':
     with open('logo.txt', 'r') as file:
@@ -24,9 +29,15 @@ if __name__ == '__main__':
     while(loop):
         print("1. Search for a movie/tv show/anime")
         print("2. Quit")
-        arg = int(input("Enter your option: "))
-        if arg == 1:
-            media = input("Enter the movie: ")
-            search_media(media)
-        elif arg == 2:
-            loop = False
+
+        try:
+            arg = int(input("Enter your option: "))
+            if arg == 1:
+                media = input("Enter the movie: ")
+                search_media(media)
+            elif arg == 2:
+                loop = False
+            else:
+                print("Invalid Option")
+        except ValueError:
+            print("must be a number")
