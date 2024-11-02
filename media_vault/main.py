19c810e3c0c996d866e2749663143eab8b0797d3
Goutham100 <gouthamjagadeesh100@gmail.com>
2024-11-02 18:15:10 +0530

    api has been substituted

--- media_vault/main.py
+++ media_vault/main.py
@@ -1,48 +1,32 @@
-import requests
+from operator import truediv
 
-API_KEY = 'd78386fe'
-BASE_URL = 'http://www.omdbapi.com/'
+from tmdbv3api import TMDb, Movie, Search
 
-def search_movie(title):
-    # parameter for the request
-    params = {
-        'apikey': API_KEY,
-        't': title,
-        'plot': 'short'  # Use 'short' for a brief summary; use 'full' for detailed summary
-    }
+tmdb = TMDb()
+tmdb.api_key = '09d0650e48d242f97c52f1e4f6780f98'
 
-    # Makes a request
-    response = requests.get(BASE_URL, params=params)
+def search_media(cinema):
 
-    # Check if request was successful
-    if response.status_code == 200:
-        data = response.json()
-        # Check if movie was found
-        if data.get("Response") == "True":
-            genre = data.get("Genre", "N/A")
-            plot = data.get("Plot", "N/A")
-            return {"Genre": genre, "Plot": plot}
-        else:
-            return "Error 404: Movie not found Nigga!"
-    else:
-        return "Error: Unable to fetch data from OMDb."
+    movie = Movie()
+    search_results = movie.search(cinema)
 
-# this acts as the main function
+    print(f"Title: {search_results[0].title}")
+    print(f"Overview: {search_results[0].overview}")
+    print(f"Release Date: {search_results[0].release_date}")
+    print(f"Rating: {search_results[0].vote_average}")
+    print()
+
 if __name__ == '__main__':
     with open('logo.txt', 'r') as file:
         content = file.read()
         print(content)
     loop = True
-    # this while displays the options for users to choose
-    while loop:
-        print('1. Find a movie/tv show/anime')
-        print('2. Quit')
-        arg = int(input("Enter your choice: "))
+    while(loop):
+        print("1. Search for a movie/tv show/anime")
+        print("2. Quit")
+        arg = int(input("Enter your option: "))
         if arg == 1:
-            movie_name = input("Enter the name of movie/tv show/anime : ")
-            movie_data = search_movie(movie_name)
-            print(movie_data)
+            media = input("Enter the movie: ")
+            search_media(media)
         elif arg == 2:
             loop = False
-        else:
-            print("Invalid Choice Dumbass Nigga")
